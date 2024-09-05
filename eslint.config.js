42ce97ae5ceb0d9c56b502a6732f069502e51803
Moritz Poßecker <M.Possecker@deskcenter.com>
2024-09-05 21:47:34 +0200

    correct lint checking

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,89 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import tsEslint from '@typescript-eslint/eslint-plugin'
+import prettier from 'eslint-config-prettier'
+import tsParser from '@typescript-eslint/parser'
 
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist', 'src/components/**'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['src/components/**'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      react,
+      '@typescript-eslint': tsEslint
+    },
+    rules: {
+      ...prettier.rules,
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          'selector': 'variableLike',
+          'format': ['camelCase'],
+          'leadingUnderscore': 'allow',
+        },
+        {
+          'selector': 'variable',
+          'modifiers': ['const'],
+          'format': ['camelCase', 'UPPER_CASE'],
+        },
+        {
+          'selector': 'function',
+          'format': ['camelCase'],
+        },
+        {
+          'selector': 'class',
+          'format': ['PascalCase'],
+        },
+        {
+          'selector': 'typeAlias',
+          'format': ['PascalCase'],
+        },
+        {
+          'selector': 'interface',
+          'format': ['PascalCase'],
+          'custom': {
+            'regex': '^I[A-Z]',
+            'match': true,
+          },
+        },
+        {
+          'selector': 'enum',
+          'format': ['PascalCase'],
+        },
+        {
+          'selector': 'enumMember',
+          'format': ['UPPER_CASE'],
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          'argsIgnorePattern': '^_',
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@/semi': ['error', 'never'],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+    },
+    settings: {
+      react: {
+        version: '18.3',
+      },
+    },
   },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-})
+  prettier,
+]
