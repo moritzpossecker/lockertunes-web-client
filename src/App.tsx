52aed1b611c3428e042b3654f391355efdf23475
Moritz Poßecker <M.Possecker@deskcenter.com>
2024-09-05 15:12:20 +0200

    add style violation on purpose

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import { useState } from 'react'
 import LockerTunesForm from '@/layouts/LockerTunesForm.tsx'
 
 const App = () => {
-  const [isConnected, setIsConnected] = useState(false)
+  const [is_connected, setIsConnected] = useState(false);
 
   return (
     <div className="bg-[url('/stadium.jpg')] bg-cover bg-fixed bg-center">
       <div className="flex flex-col items-center min-h-screen py-8 px-14 gap-8 bg-gray-900/[0.6]">
         <SpotifyAuthenticationCard
-          isConnected={isConnected}
+          isConnected={is_connected}
           setIsConnected={setIsConnected}
         />
-        {isConnected && <LockerTunesForm />}
+        {is_connected && <LockerTunesForm />}
       </div>
     </div>
   )
