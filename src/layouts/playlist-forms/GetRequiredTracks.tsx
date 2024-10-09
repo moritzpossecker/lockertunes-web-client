6d00a2ee80ad20b570f3458b036f7f9c7a41725b
Moritz Poßecker <M.Possecker@deskcenter.com>
2024-10-09 12:18:54 +0200

    commit

--- src/layouts/playlist-forms/GetRequiredTracks.tsx
+++ src/layouts/playlist-forms/GetRequiredTracks.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState, useEffect, MouseEventHandler } from 'react'
+import { debounce } from 'lodash'
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -9,9 +11,17 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form.tsx'
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandItem,
+  CommandInput,
+  CommandList,
+  CommandGroup,
+} from '@/components/ui/command.tsx'
 import { Input } from '@/components/ui/input.tsx'
 import { Button } from '@/components/ui/button.tsx'
-import { useState } from 'react'
 import { ITrack } from '@/models/ITrack.ts'
 import { useFetchTracks } from '@/lib/spotify-src/tracksApiClient.ts'
 import { MagnifyingGlassIcon, SymbolIcon } from '@radix-ui/react-icons'
@@ -32,6 +42,8 @@ const GetRequiredTracks: React.FC<IGetTracksProps> = ({
   const [open, setOpen] = useState(false)
   const [searchResults, setSearchResults] = useState<ITrack[]>([])
   const [selectedTrack, setSelectedTrack] = useState<ITrack | null>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  const popoverRef = useRef<HTMLDivElement>(null)
 
   const formSchema = z.object({
     track: z.string().min(1, 'Track selection is required'),
@@ -45,7 +57,16 @@ const GetRequiredTracks: React.FC<IGetTracksProps> = ({
     },
   })
 
-  const handleSearch = async (query: string): Promise<void> => {
+  const addTrackToList = (track: ITrack | null): void => {
+    console.log('foo')
+    console.log(track?.title)
+    if (track) {
+      setTracks([...tracks, track])
+    }
+    setSelectedTrack(null)
+  }
+
+  const handleSearch = debounce(async (query: string) => {
     if (!query) {
       setSearchResults([])
       return
@@ -56,14 +77,36 @@ const GetRequiredTracks: React.FC<IGetTracksProps> = ({
     } catch (error) {
       console.error('Error fetching tracks:', error)
     }
+  }, 500)
+
+  const handleInputFocus = (): void => {
+    setOpen(true)
   }
 
-  const addTrackToList = (track: ITrack | null): void => {
-    if (track) {
-      setTracks([...tracks, track])
+  const handleInputBlur = (): void => {
+    if (!popoverRef.current?.contains(document.activeElement)) {
+      setOpen(false)
     }
-    setSelectedTrack(null)
   }
+
+  const handlePopoverMouseDown: MouseEventHandler<HTMLDivElement> = (event) => {
+    if (!inputRef.current?.contains(event.target as Node) && !popoverRef.current?.contains(event.target as Node)) {
+      event.stopPropagation()
+      setOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    const handleDocumentClick = (event: MouseEvent): void => {
+      if (!inputRef.current?.contains(event.target as Node) && !popoverRef.current?.contains(event.target as Node)) {
+        setOpen(false)
+      }
+    }
+    document.addEventListener('click', handleDocumentClick)
+    return (): void => {
+      document.removeEventListener('click', handleDocumentClick)
+    }
+  }, [inputRef, popoverRef])
 
   return (
     <div className={card_class}>
@@ -76,12 +119,41 @@ const GetRequiredTracks: React.FC<IGetTracksProps> = ({
               <FormItem>
                 <FormLabel>Search Track</FormLabel>
                 <FormControl>
-                  <div className="flex items-center gap-2">
-                    <Input {...field} placeholder="Enter track name" className="w-full" />
-                    <Button onClick={() => handleSearch(field.value)} disabled={loading}>
-                      {loading ? <SymbolIcon className="animate-spin" /> : <MagnifyingGlassIcon/> }
-                    </Button>
-                  </div>
+                  <Command className={''}>
+                    <Input
+                      {...field}
+                      placeholder="Enter track name"
+                      className="w-full border-none outline-none focus-visible:ring-offset-0 focus-visible:ring-0"
+                      ref={inputRef}
+                      onFocus={handleInputFocus}
+                      onBlur={handleInputBlur}
+                      onInput={() => handleSearch(field.value)}
+                    />
+                    {open && (
+                      <CommandList ref={popoverRef} onMouseDown={handlePopoverMouseDown}>
+                        <CommandEmpty>No results found.</CommandEmpty>
+                        <CommandGroup>
+                          {searchResults.map((track) => (
+                            <CommandItem key={track.url}>
+                              <div
+                                onClick={() => {
+                                  addTrackToList(track)
+                                }}
+                                className="flex items-center py-2 border-b w-full">
+                                <img src={track.image_url} alt={'track cover image'} className={'h-10 w-10 rounded'} />
+                                <div className={'ml-2'}>
+                                  <div>{track.title}</div>
+                                  <div className={'text-xs text-gray-500'}>
+                                    {track.artist} • {Math.trunc(track.duration_ms / 60000)}:{Math.trunc((track.duration_ms / 1000) % 60).toString().padStart(2, '0')}
+                                  </div>
+                                </div>
+                              </div>
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </CommandList>
+                    )}
+                  </Command>
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -89,24 +161,6 @@ const GetRequiredTracks: React.FC<IGetTracksProps> = ({
           />
         </form>
       </Form>
-      <div className="mt-4">
-        {searchResults.length > 0 && (
-          <div>
-            {searchResults.map((track) => (
-              <div key={track.url} className="flex justify-between items-center py-2 border-b">
-                <div className={'flex'}>
-                  <img src={track.image_url} alt={'track cover image'} className={'h-10 w-10 rounded'} />
-                  <div className={'ml-2'}>
-                    <div>{track.title}</div>
-                    <div className={'text-xs text-gray-500'}>{track.artist} • {Math.trunc(track.duration_ms / 60000)}:{Math.trunc((track.duration_ms / 1000) % 60).toString().padStart(2, '0')}</div>
-                  </div>
-                </div>
-                <Button onClick={() => addTrackToList(track)}>Add</Button>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
       <div className="mt-4">
         {tracks.length > 0 && (
           <>
